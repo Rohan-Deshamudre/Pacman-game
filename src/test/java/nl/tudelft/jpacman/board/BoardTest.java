bf5fdcbc8d3a41542541051465f7efc366132238
Agniv Chatterjee <a.chatterjee-3@student.tudelft.nl>
2019-04-25 15:13:38 +0200

    Wrote a couple of test cases for the constructor

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,7 +1,36 @@
 package nl.tudelft.jpacman.board;
 
-public class BoardTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+class BoardTest {
+    private Board board;
 
+    /**
+     * Construct a board with a (1 x 1) grid, with one
+     * correct BasicSquare on it.
+     * Since this is a valid board, your test should pass.
+     */
+    @Test
+    void testBoard() {
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = new BasicSquare();
+        board = new Board(grid);
+        assertThat(board.invariant()).isEqualTo(true);
+    }
+
+    /**
+     * Construct a similar board to above, but with just
+     * one null square.
+     * This test should fail.
+     */
+    @Test
+    void testAnotherBoard() {
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = null;
+        board = new Board(grid);
+        assertThat(board.invariant()).isEqualTo(true);
+    }
 }
