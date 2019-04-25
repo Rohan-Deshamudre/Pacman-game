d7ebcb47d5c8e524190ece6f8e86e17a460b9267
deshamudrerohan <deshamudrerohan@gmail.com>
2019-04-25 23:14:09 +0200

    merge conflicts fix

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 /**
- * Test class for Board.
+ * Test class for the Board.
  */
 class BoardTest {
     private Board board;
