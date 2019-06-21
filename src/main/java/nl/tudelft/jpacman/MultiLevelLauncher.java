c158ec194b794c6e4bb05a2470b7ba77f981e76d
Agniv Chatterjee <a.chatterjee-3@student.tudelft.nl>
2019-06-21 15:46:42 +0200

    Fixed some checkstyle

--- src/main/java/nl/tudelft/jpacman/MultiLevelLauncher.java
+++ src/main/java/nl/tudelft/jpacman/MultiLevelLauncher.java
@@ -6,25 +6,28 @@ import nl.tudelft.jpacman.level.Level;
 
 import java.io.IOException;
 
+/**
+ * This is the class for the multi-level functionality.
+ */
 class MultiLevelLauncher extends Launcher {
-
     private MultiLevelGame multiGame;
-    public static final String stringMaps = "/level1.txt, /level2.txt, /level3.txt";
+    public static final String STRING_MAPS = "/level1.txt, /level2.txt, /level3.txt";
 
-
-
+    /**
+     * This is the getGame() method.
+     * @return the multiGame
+     */
     @Override
-    public MultiLevelGame getGame(){
+    public MultiLevelGame getGame() {
         return multiGame;
     }
-
 
     /**
      * Creates a new MultiLevelGame using the level from {@link #makeLevel()}.
      *
      * @return a new MultiLevelGame.
      */
-    public MultiLevelGame makeMultiGame() {
+    public MultiLevelGame makeGame() {
         GameFactory gf = getGameFactory();
         Level level = makeLevel();
         multiGame = gf.createSinglePlayerGame(level, loadPointCalculator());
@@ -42,6 +45,4 @@ class MultiLevelLauncher extends Launcher {
     public static void main(String[] args) throws IOException {
         new MultiLevelLauncher().launch();
     }
-
-
 }
--- src/main/java/nl/tudelft/jpacman/game/MultiLevelGame.java
+++ src/main/java/nl/tudelft/jpacman/game/MultiLevelGame.java
@@ -1,6 +1,30 @@
 package nl.tudelft.jpacman.game;
 
+import nl.tudelft.jpacman.level.Level;
+import nl.tudelft.jpacman.level.Player;
+
+import java.util.List;
+
+/**
+ * This is the class for the multi-level game.
+ */
 public class MultiLevelGame extends Game {
 
+    /**
+     * This is the constructor for multi-level game.
+     */
+    protected MultiLevelGame() {
+        //
+    }
+
+    @Override
+    public List<Player> getPlayers() {
+        //
+    }
+
+    @Override
+    public Level getLevel() {
+        //
+    }
 
 }
